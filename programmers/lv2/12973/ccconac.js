a29de91533f232871304d7452dc1438e318c31b9
sohee <106158901+ccconac@users.noreply.github.com>
2025-01-24 10:47:47 +0900

    [소희] 01/23 (#3)
    * [12939] 최댓값과 최솟값
    
    * [12924] 숫자의 표현
    
    * [12951] JadenCase 문자열 만들기
    
    * [12973] 짝지어 제거하기

---
+++ programmers/lv2/12973/ccconac.js
@@ -0,0 +1,14 @@
+function solution(s) {
+  const stack = [];
+
+  if (s.length % 2) return 0;
+
+  for (let i = 0; i < s.length; i++) {
+    const char = s.charAt(i);
+
+    if (char === stack[stack.length - 1]) stack.pop();
+    else stack.push(char);
+  }
+
+  return stack.length ? 0 : 1;
+}
