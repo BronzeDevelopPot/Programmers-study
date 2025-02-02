782c4029974c7c1ef4fa72331a20455a862cf61c
ccconac <qwerpoiu91@naver.com>
2025-02-03 03:20:38 +0900

    [43162] 네트워크

---
+++ programmers/lv3/43162/ccconac.js
@@ -0,0 +1,21 @@
+function solution(n, computers) {
+  const visited = new Array(n).fill(0);
+  let count = 0;
+
+  function dfs(node) {
+    visited[node] = 1;
+
+    for (let i = 0; i < computers.length; i++) {
+      if (!visited[i] && computers[node][i]) dfs(i);
+    }
+  }
+
+  for (let i = 0; i < n; i++) {
+    if (!visited[i]) {
+      dfs(i);
+      count += 1;
+    }
+  }
+
+  return count;
+}
